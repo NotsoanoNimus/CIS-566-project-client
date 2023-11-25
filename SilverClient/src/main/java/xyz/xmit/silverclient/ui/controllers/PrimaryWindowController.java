8093b49b7319020f6fe0f2e5e29841124a468d4e
Zack Puhl <zack.puhl@galacticadvisors.com>
2023-11-24 19:18:08 -0500

    add other panes for different window states

--- SilverClient/src/main/java/xyz/xmit/silverclient/ui/controllers/PrimaryWindowController.java
+++ SilverClient/src/main/java/xyz/xmit/silverclient/ui/controllers/PrimaryWindowController.java
@@ -3,11 +3,36 @@ package xyz.xmit.silverclient.ui.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 public final class PrimaryWindowController {
     @FXML
     public VBox mainMenuContainer;
+
+    @FXML
+    public Pane homePane;
+
+    @FXML
+    public Pane manageUsersPane;
+
+    @FXML
+    public Pane manageItemsPane;
+
+    @FXML
+    public void doHome()
+    {
+    }
+
+    @FXML
+    public void doManageUsers()
+    {
+    }
+
+    @FXML
+    public void doManageItems()
+    {
+    }
 
     @FXML
     public void doLogout()
